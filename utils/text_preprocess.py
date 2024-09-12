33d4b787ac71d48565962af05b2c4d75a479a6cc
Gianma23 <gianmaria.saggini@gmail.com>
2024-09-12 15:57:53 +0200

    continued preprocessing

--- utils/text_preprocess.py
+++ utils/text_preprocess.py
@@ -1,36 +1,25 @@
 from nltk.stem import SnowballStemmer
 import nltk
-from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 import re
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from bs4 import BeautifulSoup
 
 nltk.download('stopwords')
-nltk.download('wordnet')
 nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-
-from nltk.corpus import wordnet
-
-# def get_wordnet_pos(treebank_tag):
-#
-#     if treebank_tag.startswith('J'):
-#         return wordnet.ADJ
-#     elif treebank_tag.startswith('V'):
-#         return wordnet.VERB
-#     elif treebank_tag.startswith('N'):
-#         return wordnet.NOUN
-#     elif treebank_tag.startswith('R'):
-#         return wordnet.ADV
-#     else:
-#         return wordnet.NOUN
 
 # tokenize words
 # remove stop words
 # stemming
+class Parser(object):
+    def __call__(self, text):
+        text = text.lower()
+        text = re.sub(r'http\S+|www\.\S+', '', text)
+        text = BeautifulSoup(text, "html.parser").get_text()
+        return text
+
 stop_words = set(stopwords.words('english'))
+
 class LemmaTokenizer(object):
     def __init__(self):
         self.wnl = SnowballStemmer('english')
